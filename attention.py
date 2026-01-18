ca9101cc743dc4da38268dea59a2dfc58bd3b270
BrandonYChan <bchany100@gmail.com>
2026-01-18 10:26:42 -0500

    Initialized qkv

--- attention.py
+++ attention.py
@@ -1,43 +1,39 @@
 from dataclasses import dataclass, field
 import numpy as np
-import math
+from sequence_embed import SequenceEncoder
+from typing import Dict
 
 @dataclass
 class Attention:
-    embedding_dim: int
-    sequence_length: int
+    sequence_embedding: SequenceEncoder
     num_heads: int
     d_k: int = 0
-    _q_weights: np.ndarray = field(default_factory=lambda: np.array([]).reshape(0, 0))
-    _k_weights: np.ndarray = field(default_factory=lambda: np.array([]).reshape(0, 0))
-    _v_weights: np.ndarray = field(default_factory=lambda: np.array([]).reshape(0, 0))
+    qkv: Dict = field(default_factory=lambda: 
+                          {'q': np.array([]).reshape(0, 0), 'q_weights': np.array([]).reshape(0, 0), 
+                          'k': np.array([]).reshape(0, 0), 'k_weights': np.array([]).reshape(0, 0), 
+                          'v': np.array([]).reshape(0, 0), 'v_weights': np.array([]).reshape(0, 0)
+                          })
 
-    def _assign_qkv(self):
+    def __post_init__(self):
+        self._init_qkv
+
+    def _init_qkv(self):
         """Initialize QKV weights"""
-        self.d_k = self.embedding_dim // self.num_heads
-        bound = math.sqrt(6 / (self.embedding_dim + self.d_k))
-        
-        self._q_weights = np.random.uniform(-bound, bound, (self.embedding_dim, self.sequence_length))
-        self._k_weights = np.random.uniform(-bound, bound, (self.embedding_dim, self.sequence_length))
-        self._v_weights = np.random.uniform(-bound, bound, (self.embedding_dim, self.sequence_length))
+        self.d_k = self.sequence_embedding.embedding_dim // self.num_heads
+        bound = np.sqrt(6 / (self.sequence_embedding.embedding_dim + self.d_k))
+        embedding_dim = self.sequence_embedding.embedding_dim
+        sequence_length = self.sequence_embedding.sequence_length
 
-    def _softmax(self) -> np.ndarray:
+        for prefix in ['q' 'k', 'v']:
+            self.qkv[f"{prefix}_weights"] = np.random.uniform(-bound, bound, (embedding_dim , sequence_length))
+            self.qkv[prefix] = self.sequence_embedding.sequence_matrix @ self.qkv[f"{prefix}_weights"]
 
-        return np.array([])
+
+
+    def _softmax(self, class_logit, logits) -> np.ndarray:
+        return np.exp(class_logit) / np.sum(np.exp(logits))
 
     def _attention(self) -> np.ndarray:
 
         return np.array([])
-    
-    @property
-    def q_weights(self):
-        return self._q_weights
-    
-    @property
-    def k_weights(self):
-        return self._k_weights
-    
-    @property
-    def v_weights(self):
-        return self._v_weights
     
--- sequence_embed.py
+++ sequence_embed.py
@@ -1,13 +1,12 @@
 from dataclasses import dataclass, field
 from typing import List, Dict
 import numpy as np
-import random
 
 @dataclass
 class SequenceEncoder:
     """Encode entire sequence of tokens"""
-    embedding_dim: int
-    sequence: List[str]
+    _embedding_dim: int
+    _sequence: List[str]
     _embedding_matrix: np.ndarray = field(default_factory=lambda: np.array([]).reshape(0, 0))
     _token_matrix: np.ndarray = field(default_factory=lambda: np.array([]).reshape(0, 0))
     _position_matrix: np.ndarray = field(default_factory=lambda: np.array([]).reshape(0, 0))
@@ -17,22 +16,22 @@ class SequenceEncoder:
     def _add_token(self, token: str) -> None:
         """Add token to embedding matrix"""
         if not token in self._token_map:
-            vector = np.random.uniform(-1, 1, size = self.embedding_dim)
+            vector = np.random.uniform(-1, 1, size = self._embedding_dim)
             self._token_matrix = np.vstack([self._token_matrix, [vector]])
             self._sequence_matrix = np.vstack([self._sequence_matrix, [vector]])
             self._token_map[token] = len(self._token_matrix) - 1
         else:
             token_vector = self._token_matrix[self._token_map[token]] 
-            self._sequence_matrix = np.vstack([self._sequence_matrix, token_vector])
+            self._sequence_matrix = np.vstack([self._sequence_matrix, [token_vector]])
 
     def _add_position_encodings(self, sequence_length: int) -> None:
         """Generate sinusoidal positional encodings for positions 0 to num_positions-1."""
         positions = np.arange(sequence_length).reshape(-1, 1)
-        dimensions = np.arange(self.embedding_dim).reshape(1, -1)
-        exponent = 2 * (dimensions // 2) / self.embedding_dim
+        dimensions = np.arange(self._embedding_dim).reshape(1, -1)
+        exponent = 2 * (dimensions // 2) / self._embedding_dim
         denominator = 10000 ** exponent
 
-        encoding = np.zeros((sequence_length, self.embedding_dim))
+        encoding = np.zeros((sequence_length, self._embedding_dim))
         encoding[:, 0::2] = np.sin(positions / denominator[:, 0::2])
         encoding[:, 1::2] = np.cos(positions / denominator[:, 1::2])
         
@@ -40,10 +39,22 @@ class SequenceEncoder:
     
     def __post_init__(self) -> None:
         """Populate encoding matrices"""
-        self._add_position_encodings(len(self.sequence))
-        for token in self.sequence:
+        self._add_position_encodings(len(self._sequence))
+        for token in self._sequence:
             self._add_token(token)
         self._embedding_matrix = np.add(self._sequence_matrix, self._position_matrix)
+    
+    @property
+    def sequence(self):
+        return self._sequence
+    
+    @property
+    def embedding_dim(self):
+        return self._embedding_dim
+    
+    @property
+    def sequence_length(self):
+        return len(self._sequence)
     
     @property
     def embedding_matrix(self):
